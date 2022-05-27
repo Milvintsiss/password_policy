b140873a667fbecaf79bfa7f628eb73ba78bb702
Milvintsiss <milvintsiss@gmail.com>
2022-05-27 07:00:59 +0200

    Don't check for horizontal Unicode whitespaces by default anymore but adds option to check for it.

--- lib/src/password_validation_rules/no_space_rule.dart
+++ lib/src/password_validation_rules/no_space_rule.dart
@@ -1,15 +1,22 @@
 import '../../password_policy.dart';
 
 /// This rule verify if password contains whitespaces, newlines, tabs, vertical
-/// unicode whitespaces or horizontal Unicode whitespace. If any is present the
-/// password is not validated.
+/// unicode whitespaces. If any is present the password is not validated.
+///
+/// This rule does not check for horizontal Unicode whitespace for convenience
+/// as Flutter adds them between characters in TextFormField, if you want to
+/// check for horizontal Unicode whitespace set [doCheckForHorizontalWhitespaces]
+/// to true.
 ///
 /// [isMandatory] is true by default.
 class NoSpaceRule extends ValidationRule {
+  final bool doCheckForHorizontalWhitespaces;
+
   NoSpaceRule({
     int impact = 1,
     bool isMandatory = true,
     String? name,
+    this.doCheckForHorizontalWhitespaces = false,
   }) : super(
           impact: impact,
           mandatory: isMandatory,
@@ -17,6 +24,11 @@ class NoSpaceRule extends ValidationRule {
         );
 
   @override
-  double computeRuleScore(String password) =>
-      RegExp(r"[\s\v\h]").allMatches(password).isEmpty ? 1.0 : 0.0;
+  double computeRuleScore(String password) {
+    if (doCheckForHorizontalWhitespaces) {
+      return RegExp(r"[\s\v\h]").allMatches(password).isEmpty ? 1.0 : 0.0;
+    } else {
+      return RegExp(r"[\s\v]").allMatches(password).isEmpty ? 1.0 : 0.0;
+    }
+  }
 }
